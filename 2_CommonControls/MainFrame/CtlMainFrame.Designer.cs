706b74e842d8d94d999c4c4b24e9058bc40b5645
Oremiku <keigo.byakko2@gmail.com>
2026-08-08 19:36:20 +0900

    ヘッダーのコンポーネント化

---
+++ 2_CommonControls/MainFrame/CtlMainFrame.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ReservationApp.CommonControls.MainFrame
+{
+    partial class CtlMainFrame
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.decLine1 = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // decLine1
+            // 
+            this.decLine1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.decLine1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(90)))), ((int)(((byte)(200)))));
+            this.decLine1.Location = new System.Drawing.Point(31, 74);
+            this.decLine1.Name = "decLine1";
+            this.decLine1.Size = new System.Drawing.Size(782, 5);
+            this.decLine1.TabIndex = 9;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Instrument Sans", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(23, 26);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(0, 45);
+            this.lblTitle.TabIndex = 8;
+            // 
+            // CtlMainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(42)))), ((int)(((byte)(53)))));
+            this.Controls.Add(this.decLine1);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "CtlMainFrame";
+            this.Size = new System.Drawing.Size(830, 470);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel decLine1;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
